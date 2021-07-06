a4353ceedc3aea5f29da179755e12e005b14aaa3
Mohsin Shaikh <70101300+mohsin393@users.noreply.github.com>
2021-07-06 07:16:43 +0000

    .

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
-
-function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("booktable");
-  switching = true;
-  
-  while (switching) {
-    switching = false;
-    rows = table.rows;
-
-    for (i = 1; i < (rows.length - 1); i++)
-     {
-      shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
-
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase())
-      {
-        shouldSwitch = true;
-        break;
-      }
-    }
-
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-
-  }
-}
-
-      function bookSearch(){
+function bookSearch()
+{
       var book_id = document.getElementById("booksearch").value;
       var x =document.getElementById("booktable").rows[book_id].innerHTML;
       document.getElementById("book_id").innerHTML=x;
-      }
+}
 
-      
+
+function sortAsc() {
+            var table, rows, switching, i, x, y, shouldSwitch,colnumber;
+            table = document.getElementById("booktable");
+            colnumber=document.getElementById("colnumber").value;
+            switching = true;
+            while (switching) {
+                switching = false;
+                rows = table.rows;
+                for (i = 1; i < (rows.length - 1); i++) {
+                    shouldSwitch = false;
+                    x = rows[i].getElementsByTagName("TD")[colnumber];
+                    y = rows[i + 1].getElementsByTagName("TD")[colnumber];
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+                if (shouldSwitch) {
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                }
+            }
+        }
